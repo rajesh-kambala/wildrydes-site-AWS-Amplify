2f1d717ef4b5c488e1bf57f0edbd07fa49900835
Rajeshkambala-rk <134775968+Rajeshkambala-rk@users.noreply.github.com>
2024-12-05 12:38:58 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'ap-south-1_jEwpuaAmp', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '3r1rjajgq8ac25jqvolv64j1tu', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '6mrpl1uiug5b75beujomjhb7ea', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
