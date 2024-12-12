d9b48286023f5cab2fff0ba3128f0bdf191a4778
Rajeshkambala-rk <134775968+Rajeshkambala-rk@users.noreply.github.com>
2024-12-12 12:52:46 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-south-1_tSb2nYqCx', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '7k1s23tq5e6pgn3c3r6b93gonn', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: '', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://n7vzuez49e.execute-api.ap-south-1.amazonaws.com/Dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
