1e4e0f98b3c4f225771491245d17e19f7e98474b
rajesh-kambala <rajesh.kambala@stryv.ai>
2024-12-13 16:37:51 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'ap-south-1_HL7ktQJoI', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: 'g9vfbaro5rqvl3t4oale1bsdh', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: 'g9vfbaro5rqvl3t4oale1bsdh', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv5
         region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
